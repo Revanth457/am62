7e4fe26261ed12e6e481e5d18a4909b3323c2aaf
Revanth457 <143689680+Revanth457@users.noreply.github.com>
2024-11-13 00:34:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def index():
 
 if __name__ == '__main__':
     # Run the Flask app on all available interfaces (0.0.0.0)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='192.168.1.46', port=5000, debug=True)
